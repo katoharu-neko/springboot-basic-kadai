6a3eff7b8cde577296dc4ef9764b099fbf760d97
katoharu-neko <haruki.katoh.0@gmail.com>
2025-08-09 00:08:38 +0900

    Add files via upload

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,11 +1,13 @@
 package com.example.springkadaiform.controller;
 
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.springkadaiform.form.ContactForm;
 
@@ -14,21 +16,29 @@ public class ContactFormController {
 	
 	@GetMapping("/form")
 	public String form(Model model) {
-		model.addAttribute("contactForm", new ContactForm());
-		
-		return "contactFormView";
+        // すでにインスタンスが存在する場合は行わない
+        if (!model.containsAttribute("contactForm")) {
+            // ビューにフォームクラスのインスタンスを渡す
+            model.addAttribute("contactForm", new ContactForm());
+        }		
+        return "contactFormView";
 	}
 	
-	@PostMapping("/form")
-	public String valForm(Model model,
+	@PostMapping("/confirm")
+	public String confirm(Model model, RedirectAttributes redirectAttributes,
 			@Validated ContactForm form, BindingResult result) {
 		//バリデーションエラーがあったら終了
 		if(result.hasErrors()) {
-			return "contactFormView";
+            // フォームクラスをビューに受け渡す
+            redirectAttributes.addFlashAttribute("contactForm", form);
+            // バリデーション結果をビューに受け渡す
+            redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
+                    + Conventions.getVariableName(form), result);
+			return "redirect:/form";
 		}
-	//バリデーションに問題がなければ確認画面へ
-	model.addAttribute("contactForm", form); //---追加
-	return "confirmView";
+		//バリデーションに問題がなければ確認画面へ
+		model.addAttribute("contactForm", form); //---追加
+		return "confirmView";
 	}
 	
 
